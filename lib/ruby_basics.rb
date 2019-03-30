cf1ed28df3e0880b0dfb7934f39dcbec4413b4fa
Elle Valentine <elle.valentine@hotmail.com>
2019-03-30 09:40:25 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,43 @@
+
+
 def division(num1, num2)
+ 
+quotient = num1/num2 
+
+return quotient
  
 end
 
 def assign_variable(value)
   
-end
-
-def argue
+  name = value 
   
 end
 
-def greeting
+def argue(argument)
+  
+  return argument
+  
+end
+
+def greeting(greeting, name)
 
 end
 
 def return_a_value
+  
+  phrase = "Nice"
+  return phrase
 
 end
 
 def last_evaluated_value
-
+  phrase = "expert"
+  return phrase
 end
 
-def pizza_party
+def pizza_party(topping = "cheese")
 
+return topping
+ 
 end
